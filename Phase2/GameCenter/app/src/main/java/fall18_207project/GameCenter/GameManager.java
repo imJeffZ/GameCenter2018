aa7250cf0d434e821c2a40dfb250207b2dd7fa44
Jefferson Zhong <jefferson.zhong@mail.utoronto.ca>
2018-11-11 01:11:33 -0500

    Implemented GameManager.

---
+++ Phase2/GameCenter/app/src/main/java/fall18_207project/GameCenter/GameManager.java
@@ -0,0 +1,49 @@
+package fall18_207project.GameCenter;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/***
+ * This is the game history of Account. It stores all the games that the user saved, but have not finished.
+ * For finished games, visit ScoreBoard.
+ */
+public class GameManager implements Serializable {
+
+    ArrayList<Game> gameList;
+    private Account gameAccount;
+    public GameManager() {
+        this.gameList = new ArrayList<Game>();
+    }
+
+    private ArrayList<Game> getGameList() {
+        return this.gameList;
+    }
+
+    void addGame(Game newGame) {
+        this.gameList.add(newGame);
+    }
+
+    boolean deleteGame(Game targetGame) {
+        return this.gameList.remove(targetGame);
+    }
+
+    Game getGame(int saveId) {
+        for (Game game : this.gameList) {
+            if (game.getSaveId() == saveId) {
+                return game;
+            }
+        }
+        return null;
+    }
+
+    boolean hasGame(int saveId) {
+        for (Game game : this.gameList) {
+            if (game.getSaveId() == saveId) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
