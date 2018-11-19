52ab08e6a90033f8bbe55c94064ec03cfb8f0280
Marvel.Z <583675307@qq.com>
2018-11-19 01:46:30 -0500

    creat model class: MatchingBoard for new game: MatchingCards

---
+++ Phase2/GameCenter/app/src/main/java/fall18_207project/GameCenter/MatchingBoard.java
@@ -0,0 +1,91 @@
+package fall18_207project.GameCenter;
+
+import android.support.annotation.NonNull;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Observable;
+
+public class MatchingBoard extends Observable implements Cloneable, Serializable, Iterable<Card>  {
+
+    private int numOfColumns;
+    private  int numOfRows;
+    private  Card[][] cards;
+
+    public MatchingBoard(List<Card> cardList, int size){
+        this.numOfColumns = 4;
+        this.numOfRows = size;
+        this.cards = new Card[numOfRows][numOfColumns];
+
+        Iterator<Card> iter = cardList.iterator();
+
+        for(int row = 0; row < this.getNumOfRows(); row++){
+            for(int col = 0; col < this.getNumOfColumns(); col++)
+                this.cards[row][col] = iter.next();
+        }
+
+    }
+
+    int getNumOfColumns() {
+        return numOfColumns;
+    }
+
+    int getNumOfRows() {
+        return numOfRows;
+    }
+
+    Card getCard(int row, int col){
+        return cards[row][col];
+    }
+
+
+    int getNumOfCards(){
+        return numOfColumns*numOfRows;
+    }
+
+    void turnCard(int row, int col, boolean up){
+        cards[row][col].turn(up);
+        setChanged();
+        notifyObservers();
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Board{" +
+                "tiles=" + Arrays.toString(cards) +
+                '}';
+    }
+
+    @NonNull
+    @Override
+    public Iterator<Card> iterator() {
+        return new MatchingBoard.BoardIterator();
+    }
+
+    /**
+     * The iterator class for Board's iterator.
+     */
+    private class BoardIterator implements Iterator<Card> {
+        /**
+         * The position
+         */
+        private int position = 0;
+
+        @Override
+        public boolean hasNext() {
+            return position != numOfColumns * numOfRows;
+        }
+
+        @Override
+        public Card next() {
+            int row = position / numOfRows;
+            int col = position % numOfColumns;
+            Card nextCard = cards[row][col];
+            position++;
+            return nextCard;
+        }
+    }
+}
