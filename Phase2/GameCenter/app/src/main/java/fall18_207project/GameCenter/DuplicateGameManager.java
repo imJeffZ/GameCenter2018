b16b821d42aea54f921ad7b0018e0a329d279aee
Jefferson Zhong <jefferson.zhong@mail.utoronto.ca>
2018-11-27 11:12:54 -0500

    Generalized GameManager into a super class with abstract method addGame(). Implemented DuplicateGameManager and NonDuplicateGameManager. These two subclasses are useful when we want/don't want two games with same saveId in a gamaManager.

--- Phase2/GameCenter/app/src/main/java/fall18_207project/GameCenter/DuplicateGameManager.java
+++ Phase2/GameCenter/app/src/main/java/fall18_207project/GameCenter/DuplicateGameManager.java
@@ -4,22 +4,14 @@ package fall18_207project.GameCenter;
  * This is the game history of Account. It stores all the games that the user saved, but have not finished.
  * For finished games, visit ScoreBoard.
  */
-public class NonDuplicateGameManager extends GameManager {
+public class DuplicateGameManager extends GameManager {
 
-    public NonDuplicateGameManager() {
+    public DuplicateGameManager() {
         super();
     }
 
-
-    /***
-     * Do not allow two saved Games to have same saveId.
-     */
     @Override
     void addGame(Game newGame) {
-        if (hasGame(newGame.getSaveId())) {
-            deleteGame(newGame.getSaveId());
-        }
         this.gameList.add(newGame);
     }
-
 }
