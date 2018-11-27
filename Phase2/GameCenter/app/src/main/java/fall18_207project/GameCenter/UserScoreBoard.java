f1623b9c342ab153c9d7fc1699d1cefadbbac0a3
Jefferson Zhong <jefferson.zhong@mail.utoronto.ca>
2018-11-27 11:33:02 -0500

    Implemented UserScoreBoard, this scoreBoard records an user's all finished games.

--- Phase2/GameCenter/app/src/main/java/fall18_207project/GameCenter/UserScoreBoard.java
+++ Phase2/GameCenter/app/src/main/java/fall18_207project/GameCenter/UserScoreBoard.java
@@ -1,78 +1,84 @@
 package fall18_207project.GameCenter;
 
-import android.support.annotation.NonNull;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 /***
- * A Global ScoreBoard for all users.
- *
- * TODO: Implement extra feature: Tap on a ScoreRecord to play the game again.
+ * A user ScoreBoard.
  */
-class UserScoreBoard implements GameScoreBoard {
+class UserScoreBoard implements GameScoreBoard, Serializable {
 
     // Map of <gameId> to list of ScoreEntry. ScoreEntry is Node of userName and Game.
-    Map<Integer, ArrayList<GameRecord>> recordMap;
+//    Map<Integer, ArrayList<GameRecord>> recordMap;
 
+    // Map of <gameId> to <NonDuplicateGameManager>, each NonDuplicateGameManager is responsible for a specific type of Game.
+//    private Map<Integer, GameManager> recordMap;
+
+    private ArrayList<Game> gameRecords;
     UserScoreBoard() {
-        recordMap = new HashMap<Integer, ArrayList<GameRecord>>();
+//        recordMap = new HashMap<Integer, ArrayList<GameRecord>>();
+//        recordMap = new HashMap<Integer, GameManager>();
+        gameRecords = new ArrayList<Game>();
     }
 
-
-    public Map<Integer, ArrayList<GameRecord>> getRecordMap() {
-        return recordMap;
-    }
 
     public void updateScore(Game completedGame) {
-        int gameId = completedGame.getGameId();
-        if (!recordMap.containsKey(gameId)) {
-            recordMap.put(gameId, new ArrayList<GameRecord>());
+        gameRecords.add(completedGame);
+    }
+
+
+
+//    /***
+//     * Helper function to extract a list of GameRecords from recordMap.
+//     *
+//     * @param gameId
+//     * @return an ArrayList of sorted ScoreRecord with respect to the gameId.
+//     */
+//    ArrayList<GameRecord> getSortedGameRecord(int gameId) {
+//        ArrayList<GameRecord> gameRecordList = recordMap.get(gameId);
+//        Collections.sort(gameRecordList);
+//        return gameRecordList;
+//    }
+
+    public ArrayList<Game> getSortedGames(int gameId) {
+        ArrayList<Game> specificTypeGameList = new ArrayList<Game>();
+        for (Game g : gameRecords) {
+            if (g.getGameId() == gameId) {
+                specificTypeGameList.add(g);
+            }
         }
-        recordMap.get(gameId).add(new GameRecord(completedGame));
+        // TODO: Sort the list
+//        specificTypeGameList.sort();
+        return specificTypeGameList;
     }
 
     /***
-     * Helper function to extract a list of GameRecords from recordMap.
-     *
-     * @param gameId
-     * @return an ArrayList of sorted ScoreRecord with respect to the gameId.
+     * Basically extracting all games from user score board. Should use getSortedGames(<gameId></>)
+     * @return ArrayList<Game>
      */
-    ArrayList<GameRecord> getSortedGameRecord(int gameId) {
-        ArrayList<GameRecord> gameRecordList = recordMap.get(gameId);
-        Collections.sort(gameRecordList);
-        return gameRecordList;
+    @Deprecated ArrayList<Game> getAllGames() {
+        return gameRecords;
     }
 
-    public ArrayList<Game> getSortedGames(int gameId) {
-        ArrayList<GameRecord> gameRecordList = getSortedGameRecord(gameId);
-        ArrayList<Game> gameList = new ArrayList<Game>();
-        for (GameRecord gameRecord : gameRecordList) {
-            gameList.add(gameRecord.getGame());
-        }
-        return gameList;
-    }
-
-    /**
-     * Wrapper class to make each GameRecord comparable.
-     */
-    private class GameRecord implements Comparable<GameRecord>{
-        private Game game;
-        GameRecord(Game game) {
-            this.game = game;
-        }
-
-        public Game getGame() {
-            return this.game;
-        }
-
-        @Override
-        public int compareTo(@NonNull GameRecord o) {
-            return this.game.calculateScore() - o.getGame().calculateScore();
-        }
-    }
+//    /**
+//     * Wrapper class to make each GameRecord comparable.
+//     */
+//    private class GameRecord implements Comparable<GameRecord>{
+//        private Game game;
+//        GameRecord(Game game) {
+//            this.game = game;
+//        }
+//
+//        public Game getGame() {
+//            return this.game;
+//        }
+//
+//        @Override
+//        public int compareTo(@NonNull GameRecord o) {
+//            return this.game.calculateScore() - o.getGame().calculateScore();
+//        }
+//    }
 }
