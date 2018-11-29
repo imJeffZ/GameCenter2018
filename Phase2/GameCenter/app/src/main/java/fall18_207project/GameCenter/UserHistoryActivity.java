6d5a2b5cf738f083b2eff7bbd1c48e07340b120d
Marvel.Z <583675307@qq.com>
2018-11-29 01:30:59 -0500

    add spinner and navigation for UserHistoryActivity and connect them to different game type activity.

--- Phase2/GameCenter/app/src/main/java/fall18_207project/GameCenter/UserHistoryActivity.java
+++ Phase2/GameCenter/app/src/main/java/fall18_207project/GameCenter/UserHistoryActivity.java
@@ -3,11 +3,17 @@ package fall18_207project.GameCenter;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Spinner;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -25,26 +31,76 @@ public class UserHistoryActivity extends Activity {
     public static String userEmail = "";
     private AccountManager accountManager;
 
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_game_center:
+                    Intent tmp1 = new Intent(UserHistoryActivity.this, GameCentreActivity.class);
+                    startActivity(tmp1);
+                    break;
+                case R.id.navigation_user_history:
+                    break;
+                case R.id.navigation_global_scoreboard:
+                    Intent tmp2 = new Intent(UserHistoryActivity.this, GlobalScoreBoardActivity.class);
+                    startActivity(tmp2);
+                    break;
+            }
+            return false;
+        }
+    };
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         readAccountManagerFromSer(LoginActivity.ACCOUNT_MANAGER_DATA);
 
-        ArrayList<Game> gameList = new ArrayList<>();
+
         // TODO: Make this only show specific type of games by utilizing the getUserScoreBoard().getSortedGames(Game id) method
-        gameList = accountManager.getAccount(userEmail).getUserScoreBoard().getAllGameList();
-        Collections.reverse(gameList);
         setContentView(R.layout.activity_user_history);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Menu menu = navigation.getMenu();
+        MenuItem menuItem = menu.getItem(1);
+        menuItem.setChecked(true);
 
         ListView scoreBoardView;
         scoreBoardView = findViewById(R.id.historyView);
         final List<Map<String, Object>> list = new ArrayList<>();
-        getData(list, gameList);
+        final ArrayList<Game> finalGameList = new ArrayList<>();
+        getData(list, finalGameList,0);
         final SimpleAdapter adapter = new SimpleAdapter(this, list,
                 R.layout.user_history_item, new String[]{"gameId", "score"},
                 new int[]{R.id.user, R.id.score});
         scoreBoardView.setAdapter(adapter);
 
-        final ArrayList<Game> finalGameList = gameList;
+        Spinner spinner = findViewById(R.id.spinner);
+        String[] mItems = {"ST 3 X 3", "ST 4 x 4", "ST 5 X 5", "MC 4 x 4", "MC 5 x 5", "MC 6 x 6", "2048"};
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, mItems);
+        spinner.setAdapter(spinnerAdapter);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+                list.clear();
+                finalGameList.clear();
+                getData(list, finalGameList,pos+1);
+                adapter.notifyDataSetChanged();
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Another interface callback
+            }
+        });
+
         scoreBoardView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -53,11 +109,9 @@ public class UserHistoryActivity extends Activity {
                 selectedGame.reset();
                 accountManager.getAccount(userEmail).getAutoSavedGames().addGame(selectedGame);
                 saveToFile(LoginActivity.ACCOUNT_MANAGER_DATA);
+                int i = selectedGame.getGameId();
+                goToDifferentGames(i, selectedGame);
 
-                Intent goToGame = new Intent(getApplicationContext(), GameActivity.class);
-                goToGame.putExtra("saveType", "autoSave");
-                goToGame.putExtra("saveId", selectedGame.getSaveId());
-                startActivity(goToGame);
             }
         });
 
@@ -76,16 +130,28 @@ public class UserHistoryActivity extends Activity {
         readAccountManagerFromSer(LoginActivity.ACCOUNT_MANAGER_DATA);
     }
 
-    private void getData(List<Map<String, Object>> list, ArrayList<Game> gameArrayList) {
-
-        for (Game g: gameArrayList) {
+    private void getData(List<Map<String, Object>> list,  ArrayList<Game> playGame, int id) {
+        ArrayList<Game> gameList = accountManager.getAccount(userEmail).getUserScoreBoard().getAllGameList();
+        for (int i = 0; i < gameList.size(); i++) {
             Map<String, Object> map = new HashMap<>();
-            map.put("gameId", g.gameId);
-            map.put("score", g.calculateScore());
-            list.add(map);
+            if (gameList.get(i).gameId ==id){
+                playGame.add(gameList.get(i));
+                map.put("gameId", gameList.get(i).gameId);
+                map.put("score",  gameList.get(i).saveId);
+                list.add(map);
+            }
         }
     }
 
+    private void goToDifferentGames(int id, Game selectedGame){
+
+        Intent goToGame = id <= 3? new Intent(getApplicationContext(), GameActivity.class):
+                id <= 6? new Intent(getApplicationContext(), MatchingCardsGameActivity.class):
+                        new Intent(getApplicationContext(), Game2048Activity.class);
+        goToGame.putExtra("saveType", "autoSave");
+        goToGame.putExtra("saveId", selectedGame.getSaveId());
+        startActivity(goToGame);
+    }
 
     private void readAccountManagerFromSer(String fileName) {
 
